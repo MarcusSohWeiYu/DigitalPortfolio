010db641f448a073e7480d5779f28e7d187cf111
learncodingwithme1231 <73458943+MarcusSohWeiYu@users.noreply.github.com>
2024-03-10 16:44:57 +0800

    Implemented Contact Form:

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//To download resume on phone
 document.getElementById('downloadCvLink').addEventListener('click', function (e) {
   // Check if the user is on a mobile device
   if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
@@ -8,4 +9,38 @@ document.getElementById('downloadCvLink').addEventListener('click', function (e)
     window.open('Resume/Soh-Wei-Yu_Resume.pdf', '_blank');
   }
   // For desktop users, the default "download" attribute behavior will proceed
+});
+
+//Submitting Contact Form
+document.getElementById('contact-form').addEventListener('submit', function(e) {
+  e.preventDefault();
+
+  // Form data as an object
+  let formData = new FormData(this);
+
+  fetch('https://formspree.io/f/xvoeqqlr', {
+    method: 'POST',
+    body: formData,
+    headers: {
+      'Accept': 'application/json'
+    },
+  })
+  .then(response => {
+    if (response.ok) {
+      document.getElementById('form-response-succeed').textContent = 'Thanks for your submission!';
+      // Optionally, reset the form here
+      document.getElementById('contact-form').reset();
+    } else {
+      response.json().then(data => {
+        if (Object.hasOwn(data, 'errors')) {
+          document.getElementById('form-response-fail').textContent = data.errors.map(error => error.message).join(", ");
+        } else {
+          document.getElementById('form-response-fail').textContent = 'Oops! There was a problem with your submission.';
+        }
+      });
+    }
+  })
+  .catch(error => {
+    document.getElementById('form-response-fail').textContent = 'Oops! There was a problem with your submission.';
+  });
 });
