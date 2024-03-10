084d6dfac309b7b887527cdb1ee7541054c9104b
learncodingwithme1231 <73458943+MarcusSohWeiYu@users.noreply.github.com>
2024-03-10 14:52:28 +0800

    Adjusted Layout

--- script.js
+++ script.js
@@ -1,32 +1,13 @@
-(function () {
-  "use strict";
-
-  var carousels = function () {
-    $(".owl-carousel1").owlCarousel({
-      loop: true,
-      center: true,
-      margin: 0,
-      responsiveClass: true,
-      nav: false,
-      responsive: {
-        0: {
-          items: 1,
-          nav: false
-        },
-        680: {
-          items: 2,
-          nav: false,
-          loop: false
-        },
-        1000: {
-          items: 3,
-          nav: true
-        }
-      }
-    });
-  };
-
-  (function ($) {
-    carousels();
-  })(jQuery);
-})();
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  grabCursor: true,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
