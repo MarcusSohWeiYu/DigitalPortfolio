b4203e2c997853501d87f37ee518815f7d0eb4e4
learncodingwithme1231 <73458943+MarcusSohWeiYu@users.noreply.github.com>
2024-07-17 18:29:49 +0800

    rebranded portfolio

--- download.php
+++ download.php
@@ -1,5 +1,5 @@
 <?php
-$file = 'Resume/Soh-Wei-Yu_IT_Resume_V1.pdf'; // Adjust the path to your PDF file
+$file = 'Resume/SohWeiYu_Resume.pdf'; // Adjust the path to your PDF file
 header('Content-Description: File Transfer');
 header('Content-Type: application/pdf');
 header('Content-Disposition: attachment; filename="'.basename($file).'"');
